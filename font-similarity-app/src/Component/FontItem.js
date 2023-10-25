c46c1f8fc71ca53ceba8bdeca0b69c85d9847632
SCM <hys1693359@gmail.com>
2023-10-25 11:36:04 +0900

    23.10.25 (수) : 유사도 제거

--- font-similarity-app/src/Component/FontItem.js
+++ font-similarity-app/src/Component/FontItem.js
@@ -7,7 +7,7 @@ function FontItem({font, isPaid}) {
     return (
         <div className="font-item">
             <strong>{font.fontName}</strong>
-            <span>유사도 : {font.similarity}</span>
+            {/*<span>유사도 : {font.similarity}</span>*/}
             <img src={font.fontImage} alt={font.fontName} />
             <a href={font.fontDownloadLink} download>Download {font.fontName}</a>
             <div className="font-type">
